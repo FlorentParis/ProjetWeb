eea0f7f0cb28f9499cd3a5102f21520c69760c64
Florent <florentparis28@gmail.com>
2021-02-01 16:37:44 +0100

    Jaken init

---
+++ Jaken/script.js
@@ -0,0 +1,9 @@
+const btnCiseaux = document.getElementById("ciseaux");
+const btnPierre = document.getElementById("pierre");
+const btnFeuille = document.getElementById("feuille");
+
+var sommeJoueur = 1 + 2;
+document.getElementById("resultJoueur").innerHTML=sommeJoueur;
+
+var sommeOrdi = 1 + 1;
+document.getElementById("resultOrdi").innerHTML=sommeOrdi;
