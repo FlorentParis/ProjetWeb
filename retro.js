51acb56f915248156168019540684bb3fd65a17d
Florent <florentparis28@gmail.com>
2021-02-12 23:13:33 +0100

    bug sound

--- retro.js
+++ retro.js
@@ -8,6 +8,8 @@ let loader = document.getElementById("loader");
 let test = 0;
 const btnsound = document.getElementById("btnsound");
 
+let etat = false;
+
 /* Init l1,l2,...l7 puis Init Liste */
 let l1 = document.getElementById("l1");
 let l2 = document.getElementById("l2");
@@ -24,6 +26,7 @@ function sleep(ms) {
 }
 
 async function demo(){
+  etat = false;
   while(test<11){
     var time = Math.floor(Math.random(2) * Math.floor(5));
     cursor = 10*test;
@@ -74,8 +77,6 @@ function ajout(game) {
     alert("bug");
   }
 }
-
-let etat = false;
 
 function ajoutSound() {
   if(etat){
